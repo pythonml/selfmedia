b606d713ae0b8eb92beb2e1e061a99970e0703fc
Zhongqiang Shen <shenzhongqiang@msn.com>
2018-08-13 21:45:35 +0800

    make it more robust

--- convert.py
+++ convert.py
@@ -13,120 +13,115 @@ class Block(object):
         return re.sub(r'_', r'\_', text)
 
     @staticmethod
-    def convert_anchor(html):
-        patt = re.compile(r'<a href="([^"]+)"[^>]*>(.*?)</a>')
-        matched = patt.search(html)
-        if matched:
-            url = matched.group(1)
-            name = matched.group(2)
-            markdown = "[%s](%s)" % (name, url)
-            new_html = patt.sub(markdown, html)
-            return Block.convert_anchor(new_html)
-        return html
-
-    @staticmethod
-    def convert_bold(html):
-        patt = re.compile(r'<b>(.*?)</b>', re.M|re.S)
-        matched = patt.search(html)
-        if matched:
-            text = matched.group(1)
-            markdown = '**%s**' % text
-            return markdown
-        return html
-
-    @staticmethod
-    def parse_figure(element):
-        for e in element:
-            if e.tag == "img" and e.attrib["class"] == "origin_image zh-lightbox-thumb lazy":
-                return e.attrib["data-original"]
-
-    @staticmethod
-    def parse_code(element):
-        return element.text_content()
-
-    @staticmethod
-    def parse_paragraph(element):
-        raw_html = etree.tostring(element).decode("utf-8")
-        md_html = Block.convert_anchor(raw_html)
-        md_html = Block.escape_underscore(md_html)
-        node = html.fromstring(md_html)
-        return node.text_content()
-
-    def parse_ol(element):
-        result = []
-        for child in element:
-            result.append(child.text)
-        return result
-
-    def parse_ul(element):
-        result = []
-        for child in element:
-            result.append(child.text)
-        return result
-
-    def parse_blockquote(element):
-        raw_html = etree.tostring(element).decode("utf-8")
-        md_html = Block.convert_bold(raw_html)
-        node = html.fromstring(md_html)
-        return node.text_content()
-
-    @staticmethod
-    def parse_head(element):
-        return element.text
-
-    def __init__(self, element):
-        self.element = element
+    def html2md(element):
         # figure
         if element.tag == "figure":
-            self.type = "figure"
-            self.data = Block.parse_figure(element)
-        # code
-        elif element.tag == "div" and element.attrib["class"] == "highlight":
-            self.type = "code"
-            self.data = Block.parse_code(element)
-        # head
-        elif element.tag == "h2":
-            self.type = "head"
-            self.data = Block.parse_head(element)
-        # html
-        elif element.tag == "p":
-            self.type = "paragraph"
-            self.data = Block.parse_paragraph(element)
-        elif element.tag == "ol":
-            self.type = "ol"
-            self.data = Block.parse_ol(element)
-        elif element.tag == "ul":
-            self.type = "ul"
-            self.data = Block.parse_ul(element)
-        elif element.tag == "blockquote":
-            self.type = "blockquote"
-            self.data = Block.parse_blockquote(element)
+            imgs = element.xpath('.//img')
+            if len(imgs) == 0:
+                raise Exception("error getting image")
+            img = imgs[0]
+            img_url = ""
+            if "data-original" in img.attrib:
+                img_url = img.attrib["data-original"]
+            else:
+                img_url = img.attrib["src"]
+            md = "![]({})".format(img_url)
+            return md
 
-    def to_markdown(self):
-        if self.type == "paragraph":
-            return self.data
-        elif self.type == "head":
-            html = "#### %s" % self.data
-            return html
-        elif self.type == "code":
-            html = "```python\n%s\n```\n" % self.data
-            return html
-        elif self.type == "figure":
-            html = "![](%s)" % self.data
-            return html
-        elif self.type == "ol":
-            html = ""
-            for i in range(len(self.data)):
-                html += "%d. %s\n" % (i, self.data[i])
-            return html
-        elif self.type == "ul":
-            html = ""
-            for i in range(len(self.data)):
-                html += "* %s\n" % (self.data[i])
-            return html
-        elif self.type == "blockquote":
-            html = "> %s\n" % self.data
-            return html
+        # code
+        if element.tag == "div" and element.attrib["class"] == "highlight":
+            code = element.text_content()
+            lang_class = element.xpath('./pre/code')[0].attrib["class"]
+            matched = re.match("language-(.*)$", lang_class)
+            lang = matched.group(1)
+            md = '```{}\n{}\n```'.format(lang, code)
+            return md
+        if element.tag == "code":
+            code = element.text
+            md = '`{}`'.format(code)
+            return md
+
+        # head
+        if element.tag == "h2":
+            text = Block.escape_underscore(element.text)
+            md = "#### {}".format(text)
+            return md
+
+        # html
+        if element.tag == "p":
+            md = "" if element.text is None else Block.escape_underscore(element.text)
+            for node in element:
+                node_md = Block.html2md(node)
+                md += node_md
+                node_tail = "" if node.tail is None else Block.escape_underscore(node.tail)
+                md += node_tail
+            return md
+
+        if element.tag == "ol":
+            md = ""
+            i = 1
+            for node in element:
+                node_md = Block.html2md(node)
+                md += "{}. {}\n".format(i, node_md)
+                i += 1
+            return md
+
+        if element.tag == "ul":
+            md = ""
+            for node in element:
+                node_md = Block.html2md(node)
+                md += "* {}\n".format(node_md)
+            return md
+
+        if element.tag == "hr":
+            md = "---\n"
+            return md
+
+        if element.tag == "b":
+            md = "" if element.text is None else Block.escape_underscore(element.text)
+            for node in element:
+                node_md = Block.html2md(node)
+                md += node_md
+                node_tail = "" if node.tail is None else Block.escape_underscore(node.tail)
+                md += node_tail
+            md = "**{}**".format(md)
+            return md
+
+        if element.tag == "i":
+            md = "" if element.text is None else Block.escape_underscore(element.text)
+            for node in element:
+                node_md = Block.html2md(node)
+                md += node_md
+                node_tail = "" if node.tail is None else Block.escape_underscore(node.tail)
+                md += node_tail
+            md = "*{}*".format(md)
+            return md
+
+        if element.tag == "a":
+            url = element.attrib["href"]
+            name_md = ""
+            if len(element) > 0:
+                name_md = Block.html2md(element[0])
+            else:
+                name_md = Block.escape_underscore(element.text)
+            md = "[{}]({})".format(name_md, url)
+            return md
+
+        if element.tag == "br":
+            md = "\n"
+            return md
+
+        if element.tag == "blockquote":
+            md = "" if element.text is None else Block.escape_underscore(element.text)
+            for node in element:
+                node_md = Block.html2md(node)
+                md += node_md
+                node_tail = "" if node.tail is None else Block.escape_underscore(node.tail)
+                md += node_tail
+            md = "> {}".format(md)
+            return md
+
+        raise Exception("unrecognized tag {}".format(element.tag))
 
     def __repr__(self):
         return '<Block type="%s" data="%s">' % (self.type, self.data)
@@ -140,6 +135,7 @@ class Article(object):
         self.title_image = title_image
         self.author_name = author_name
         self.content = content
+        self.markdown = ""
 
     def parse_title(self, element):
         data = json.loads(element.attrib["data-zop"])
@@ -151,21 +147,12 @@ class Article(object):
         else:
             self.title_image = imgs[0].attrib["src"]
 
-    def parse_article(self, element):
-        result = []
+    def to_markdown(self, element):
+        result = ""
         for e in element:
-            block = Block(e)
-            result.append(block)
-        self.content = result
-
-    def to_markdown(self):
-        blocks = self.content
-        content_html = ""
-        for block in blocks:
-            content_html += block.to_markdown()
-            content_html += "\n"
-        html = content_html
-        return html
+            md = Block.html2md(e)
+            result = result + md + "\n\n"
+        self.markdown = result
 
     def __repr__(self):
         name = "<Article [title=\"%s\", title_image=\"%s\", author_name=\"%s\"]>" % (self.title.encode("utf-8"),
@@ -189,7 +176,7 @@ def get_zhihu_content(url):
     article.parse_title(content_div)
 
     article_div = content_div.xpath('.//div[contains(@class, "Post-RichText")]')[0]
-    article.parse_article(article_div)
+    article.to_markdown(article_div)
 
     return article
 
@@ -199,7 +186,7 @@ if len(sys.argv) < 2:
 
 
 url = sys.argv[1]
-#url = "https://zhuanlan.zhihu.com/p/32961280"
+#url = "https://zhuanlan.zhihu.com/p/41694635"
 article = get_zhihu_content(url)
-print(article.to_markdown())
+print(article.markdown)
 
